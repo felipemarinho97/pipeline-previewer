be9831e97cc6cda2e3dbb1175dc37a1cb38e3750
Felipe Marinho <felipevm97@gmail.com>
2019-04-11 14:25:50 -0300

    add a error handler

--- controllers/api.js
+++ controllers/api.js
@@ -6,6 +6,13 @@ if (!config.multiProjects) {
   defaultProject = config.projects.find(proj => proj.name === config.default);
 }
 
+if (defaultProject == null) {
+  console.error(
+    'Error: Please provide a config.json file with the repository data.'
+  );
+  process.exit(-1);
+}
+
 module.exports = function(app) {
   if (!config.multiProjects) {
     if (defaultProject.provider === 'gitlab') {
